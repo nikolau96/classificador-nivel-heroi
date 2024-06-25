e34472d9d6124189a1e62e5adef69197c231901f
Nickolas Markus <113360684+nikolau96@users.noreply.github.com>
2024-06-25 15:21:03 -0300

    Substituição na verificação de condição de IF para Switch/Case

--- index.js
+++ index.js
@@ -2,31 +2,34 @@ var inputNome = "Digite seu nome: ";
 const nome = inputNome;
 var inputXP = "Informe o seu XP: ";
 const xp = parseInt(inputXP);
+
 var level = "";
 
-if(xp < 1000){
-    level = "Ferro";
-}
-if(xp > 1000 && xp <= 2000){
-    level = "Bronze";
-}
-if(xp > 2000 && xp <= 5000){
-    level = "Prata";
-}
-if(xp > 5000 && xp <= 7000){
-    level = "Ouro";
-}
-if(xp > 7000 && xp <= 8000){
-    level = "Platina";
-}
-if(xp > 8000 && xp <= 9000){
-    level = "Ascendente";
-}
-if(xp > 9000 && xp <= 10000){
-    level = "Imortal";
-}
-if(xp > 10000){
-    level = "Radiante";
+switch(true){
+    case (xp < 1000):
+        level = "Ferro";
+        break;
+    case (xp > 1000 && xp <= 2000):
+        level = "Bronze";
+        break;
+    case (xp > 2000 && xp <= 7000):
+        level = "Prata";
+        break;
+    case (xp > 5000 && xp <= 7000):
+        level = "Ouro"
+        break;
+    case (xp > 7000 && xp <= 8000):
+        level = "Platina";
+        break;
+    case (xp > 8000 && xp <= 9000):
+        level = "Ascendente";
+        break;
+    case (xp > 9000 && xp <= 10000):
+        level = "Imortal";
+        break;
+    case (xp > 10000):
+        level = "Radiante";
+        break;
 }
 
 console.log("O herói de nome " + nome + " está no nível " + level);
